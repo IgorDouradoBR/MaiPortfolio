c316dc9097e554665898a0a2d1f7b7594e17e52c
IgorDouradoBR <igor.d@edu.pucrs.br>
2024-12-06 04:59:58 -0300

    dwdx

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,12 +30,12 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+            <Col md={5} style={{ paddingBottom: 20, paddingLeft: 30 }}>
               <img
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "550px" }}
+                style={{ maxHeight: "450px" }}
               />
             </Col>
           </Row>
